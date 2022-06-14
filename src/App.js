8feb9b37da902204150e40b754a3f33db436c96d
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-06-14 12:41:09 -0500

    Finishing the style for each component.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { TodoItem } from "./TodoItem";
 const todos = [
   { text: 'Cortar cebolla', completed: false },
   { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false }
+  { text: 'Llorar con la llorona', completed: true }
 ]
 
 function App() {
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,7 +5,7 @@ function TodoItem(props) {
     return (
         <li className="TodoItem">
             <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
-            <p className={`TodoItem-p ${props.copleted && 'TpdoItem-p--complete'}`}>{props.text}</p>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
             <span className="Icon Icon-delete">X</span>
         </li>
     )
