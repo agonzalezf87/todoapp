dabfe8b78cc4a782b131f3b6319d445329ac9bbd
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-06-14 13:00:25 -0500

    Added events to items, search and create button.

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import './CreateTodoButton.css'
 
 function CreateTodoButton() {
+    const onClickButton = (msg) => {
+        alert(msg)
+    }
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className="CreateTodoButton"
+            onClick={() => onClickButton("Here goes the modal")}
+        >
+            +
+        </button>
     )
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,11 +2,31 @@ import React from "react";
 import './TodoItem.css'
 
 function TodoItem(props) {
+    const onComplete = () => {
+        alert(`Your task '${props.text}' is completed.`)
+    }
+    
+    const onDelete = () => {
+        alert(`You've deleted the task '${props.text}'`)
+    }
+
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >
+                √
+            </span>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>
+                {props.text}
+            </p>
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
+                X
+            </span>
         </li>
     )
 }
