0260aca049fd62e86c159ccd1894ab5885545b7b
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-06-13 22:27:29 -0500

    Added styles to components.

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,9 @@
 import React from "react"
+import './TodoCounter.css'
 
 function TodoCounter () {
     return (
-        <h2>You've completed 2 of 3 tasks</h2>
+        <h2 className="TodoCounter">You've completed 2 of 3 tasks</h2>
     )
 }
 
